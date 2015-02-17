9e2aa1cfae234f706b4b3f582f21ce3278fdd197
Hashcode <hashcode0f@gmail.com>
2015-02-16 21:58:22 -0800

    wifi: fix calibrator / race condition when doing clean install
    Change-Id: I18031f4b22cccbf1eb26346f51f5b3b9d333a244

--- prebuilt/bin/fix-mac.sh
+++ prebuilt/bin/fix-mac.sh
@@ -5,17 +5,17 @@ ORIG_NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
 NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 
 if [ ! -f "$NVS_BIN" ]; then
+    # be sure this wasn't run manually with the module loaded
+    rmmod wl12xx_sdio
     mount -o remount,rw /system
+    wait 1
     cp ${ORIG_NVS_BIN} ${NVS_BIN}
     MAC=`cat /rom/devconf/MACAddress | sed 's/\(..\)\(..\)\(..\)\(..\)\(..\)/\1:\2:\3:\4:\5:/'`
+    calibrator set nvs_mac $NVS_BIN $MAC
     chmod 644 ${NVS_BIN}
     mount -o remount,ro /system
+    wait 1
 fi
 
-#fix race condition where wl1271-nvs.bin wasn't ready after a complete wipe
-insmod /system/lib/modules/compat.ko
-insmod /system/lib/modules/cfg80211.ko
-insmod /system/lib/modules/mac80211.ko
-insmod /system/lib/modules/wl12xx.ko
 insmod /system/lib/modules/wl12xx_sdio.ko
 
